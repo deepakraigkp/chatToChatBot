5fa263b4958a9edf2eca2086ce19d7dfd278448f
deepak <deepakr@paisabazaar.com>
2018-09-25 00:00:23 +0530

    configured for heroku

--- server/server.js
+++ server/server.js
@@ -4,9 +4,10 @@ const express  = require('express');
 const publicPath = path.join(__dirname, '../public');
 var app = express();
 app.use(express.static(publicPath));
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log('publicPath-->', publicPath);
+app.listen(port, () => {
+    console.log(`Server is running on port - ${port}`);
 })
 
 
